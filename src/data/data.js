9446c4524226ac357844a1191366fa4979193205
Khalid Saifullah <khalideofficial@gmail.com>
2021-03-20 00:53:34 +0600

    Blog Details Content Implement

---
+++ src/data/data.js
@@ -0,0 +1,82 @@
+export const data = {
+  blocks: [
+    {
+        "type": "header",
+        "data": {
+            "text": "Accessibility",
+            "level": 2
+        }
+    },
+    {
+        "type": "paragraph",
+        "data": {
+            "text": "The&nbsp;<code>Tab</code>&nbsp;component should be wr<mark class=\"cdx-marker\">apped in a&nbsp;<code>Tablist</code>&nbsp;or&nbsp;<code>TabNavigation</code></mark><code></code>&nbsp;<b><i>component</i></b> to comply to&nbsp;<a href=\"http://accessibility.athena-ict.com/aria/examples/tabpanel2.shtml\">WAI-ARIA</a>&nbsp;accessibility standards."
+        }
+    },
+    {
+        "type": "paragraph",
+        "data": {
+            "text": "If you are not using a link (<code>a</code>&nbsp;tag) for your Tab, make sure to wrap in a&nbsp;<code>Tablist</code>, see examples below."
+        }
+    },
+    {
+        "type": "code",
+        "data": {
+            "code": "\n  {['Traits', 'Event History', 'Identities'].map((tab, index) =&gt; (\n    \n      {tab}\n    \n  ))}\n"
+        }
+    },
+    {
+        "type": "header",
+        "data": {
+            "text": "Tab&nbsp;Props",
+            "level": 2
+        }
+    },
+    {
+        "type": "paragraph",
+        "data": {
+            "text": "This component composes&nbsp;<code>../typography</code>"
+        }
+    },
+    {
+        "type": "paragraph",
+        "data": {
+            "text": "<code>onSelectfunc</code>Function triggered when tab is selected.<code>isSelectedbool</code>When true, the tab is selected.<code>appearancestring</code>The appearance of the tab. The default theme only comes with a default style.<code>classNamestring</code>Class name passed to the Tab. <code class=\"inline-code\">Only use this if you know what you are doing.</code>"
+        }
+    },
+    {
+        "type": "header",
+        "data": {
+            "text": "Sidebar tabs",
+            "level": 1
+        }
+    },
+    {
+        "type": "embed",
+        "data": {
+            "service": "youtube",
+            "source": "https://youtu.be/gviAd_TlhIQ?list=RDgQN1p8Ywtic",
+            "embed": "https://www.youtube.com/embed/gviAd_TlhIQ?list=RDgQN1p8Ywtic",
+            "width": 580,
+            "height": 320,
+            "caption": ""
+        }
+    },
+    {
+        "type": "simpleImage",
+        "data": {
+            "url": "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4xdax",
+            "caption": "",
+            "withBorder": false,
+            "withBackground": false,
+            "stretched": false
+        }
+    },
+    {
+        "type": "delimiter",
+        "data": {}
+    }
+]
+};
+
+
--- src/screens/BlogDetails.js
+++ src/screens/BlogDetails.js
@@ -1,18 +1,73 @@
-import {
-    useParams
-} from "react-router-dom";
+import { useParams } from "react-router-dom";
+import edjsParser from "editorjs-parser"
+import { useState, useEffect } from "react";
 
 import Navbar from "../components/Navbar"
 import blogs from "../data/blogs"
 import "../styles/BlogDetails.css"
+import { data } from "../data/data";
 
 function BlogDetails() {
     const { id } = useParams()
-
-    console.log(id)
+    const [htmlTemplate, setHtmlTemplate] = useState(null)
 
     const blog = (blogs.filter(v => v.id === Number(id)))[0]
-    console.log(blog)
+
+    const config = {
+        image: {
+          use: "figure",
+          // use figure or img tag for images (figcaption will be used for caption of figure)
+          // if you use figure, caption will be visible
+          imgClass: "img", // used class for img tags
+          figureClass: "fig-img", // used class for figure tags
+          figCapClass: "fig-cap", // used class for figcaption tags
+          path: "absolute",
+          // if absolute is passed, the url property which is the absolute path to the image will be used
+          // otherwise pass a relative path with the filename property in <> like so: '/img/<fileName>'
+        },
+        paragraph: {
+          pClass: "paragraph", // used class for paragraph tags
+        },
+        code: {
+          codeBlockClass: "code-block", // used class for code blocks
+        },
+        embed: {
+          useProvidedLength: false,
+          // set to true if you want the returned width and height of editorjs to be applied
+          // NOTE: sometimes source site overrides the lengths so it does not work 100%
+        },
+        quote: {
+          applyAlignment: false,
+          // if set to true blockquote element will have text-align css property set
+        },
+    };
+
+    const customParsers = {
+        simpleImage: function(data){
+            return `<img src="${data.url}" alt="${data.caption}" >`;
+        },
+        embed: function(data){
+            return `<iframe src="${data.embed}" width="100%" height="500px"></iframe>`;
+        },
+        code: function(data){
+            return `<pre class="codeBlock language-javascript"><code class="code">${data.code}</code></pre>`;
+        },
+        delimiter: function(){
+            return `<div class="ce-block__content"><div class="ce-delimiter cdx-block"></div></div>`
+        }
+    }
+
+    const parser = new edjsParser(config, customParsers);
+
+    function createMarkup() {
+        return {__html: htmlTemplate};
+    }
+
+    useEffect(() => {
+        setHtmlTemplate(parser.parse(data))
+        console.log(parser.parse(data))
+    }, [])
+
     if(blog){
         return (
             <>
@@ -38,8 +93,8 @@ function BlogDetails() {
                             ))}
                         </div>
                     </div>
-                    <div className="blogDetails__content"></div>
                 </div>
+                <div className="blogDetails__content" dangerouslySetInnerHTML={createMarkup()}></div>
             </div>
             </>
         )
